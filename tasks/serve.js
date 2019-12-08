db124de9bf280ff57e0bf575efb23731a36e300d
creonbabydragon <creon@babydraggie.com>
2019-12-08 17:33:20 -0600

    :heavy_minus_sign: Remove grunt-serve (See #1)

--- tasks/serve.js
+++ tasks/serve.js
@@ -5,17 +5,6 @@ const { dist } = project.paths
 // Grunt code
 module.exports = grunt => {
   grunt.registerTask('serve', 'Serve the project', () => {
-    grunt.config.merge({
-      serve: {
-        options: {
-          path: `/${dist}`,
-          port: 9000,
-        },
-      },
-    })
-
-    // Run the server
-    grunt.loadNpmTasks('grunt-serve')
-    grunt.task.run('serve')
+    grunt.config.merge({})
   })
 }
