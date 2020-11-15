1bf3c3d3d8fdaa59471d40b76bedcc2b0b3d218a
Creon The Draggie <creon@babydraggie.com>
2020-11-15 14:04:02 -0600

    :fire: Simplify Cleaning Task

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,16 @@
 // build:js        × tasks/process-javascript.js
 // build:pages     × tasks/build-pages.js
 // build:sass      × tasks/process-sass.js
-// clean           × tasks/clean.js
 // copy:files      × tasks/copy-files.js
 // minify:html     × tasks/minify-html.js
 // postprocess:css × tasks/post-process-css.js
 
 module.exports = grunt => {
   grunt.loadTasks('./tasks')
+
+  grunt.registerTask('clean', 'Remove the dist directory', () => {
+    grunt.file.delete('./dist')
+  })
 
   grunt.registerTask('build:css', ['build:sass', 'postprocess:css'])
   grunt.registerTask('build:html', ['build:pages', 'minify:html'])
